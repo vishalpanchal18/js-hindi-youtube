72499022012e4d262eda2eda9a81c11eb0c15171
vishalpanchal18 <vishalpanchal.offical@gmail.com>
2025-11-25 00:44:06 +0000

    container config done

---
+++ 01_basics/03_conservationOpertor.js
@@ -0,0 +1,33 @@
+//let score =    "33abc"  ---->33
+//let score =    null;  --------> 0
+//let score =    true; -----> 1 , false --->0
+//let score =    "Vishal"; -----> nan
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//let isLoggedIn =1; -----> true false ----- 0
+//let isLoggedIn ="vishal"; -----> true
+//let isLoggedIn =""; ------> false
+//  let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log (booleanisLoggedIn);
+
+//let someNumber =   33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+///////////////operator/////////////////
+
+// document.........mdn web docs
+let gameChanger =100;
+++gameChanger;
+console.log(gameChanger);
+
+
---
+++ 01_basics/05_datatype_Summary.js
@@ -0,0 +1,15 @@
+//#Primitive Datatype
+// we have copy of the data and we change that copy 
+
+// 7 types : String , Number , boolean , Null ,undefined , Symbol , BigInt
+
+//---------> javascript dynamic type lang or static type lang?
+const score = 10;
+
+
+
+//#Refrence (Non Primitive)
+
+// memory m refrence directly allocate kiya ja sakta hai .
+
+//Array , Object , Function 
